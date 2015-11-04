9fae313994e15b8b41c4269c78ead72895e1acc0
feeter <josigna.cp@gmail.com>
2015-11-04 01:20:36 -0300

    se elimina arriendo, compra y venta de negocio para agregar un solo modulo de Transaccion donde albergara esos movimientos, ademas se crea el ReservaDAO para comenzar a aplicar las reservas de un cliente logeado

---
+++ src/Datos/ReservaDAO.java
@@ -0,0 +1,9 @@
+package Datos;
+
+public class ReservaDAO {
+	public int RealizarReserva(){
+		
+		//exec [WEB_INS_RealizarReserva] '<parametros><Usuario>1</Usuario><Propiedad>1</Propiedad><Comentario>Comprar propiedad</Comentario><TipoReserva>C</TipoReserva></parametros>'
+		return 0;
+	}
+}
---
+++ src/Negocio/Transaccion.java
@@ -0,0 +1,6 @@
+package Negocio;
+
+//Clase para aplicar reglas de negocio sobre la compra, arriendo, reserva y corretaje sobre una propiedad
+public class Transaccion {
+
+}
